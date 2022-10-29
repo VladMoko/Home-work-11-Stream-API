fe5501a9c203bebb40dbfdf6392390cffc46f8ad
m <advmoney2233@gmail.com>
2022-10-29 19:21:24 +0300

    Commit

---
+++ src/streamZIP/JavaStream.java
@@ -0,0 +1,31 @@
+package streamZIP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JavaStream {
+        public static void main(String[] args) {
+            Stream<String> first = Stream.of("Alfa", "Beta", "Gama");
+            Stream<String> second = Stream.of("1", "2", "3", "4");
+
+            Stream<String> result = zip(first, second);
+            result.peek(System.out::println).collect(Collectors.toList());
+        }
+
+
+        public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+            List<T> collect = first.toList();
+            List<T> collect1 = second.toList();
+
+            int min = Math.min(collect.size(), collect1.size());
+            List<T> result = new ArrayList<>();
+            for (int i = 0; i < min; i++) {
+                result.add(collect.get(i));
+                result.add(collect1.get(i));
+            }
+            return result.stream();
+        }
+    }
+
